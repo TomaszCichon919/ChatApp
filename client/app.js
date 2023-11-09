d61c5b331f2fe923c44cc7b990a17a8c985792de
Tomasz Cichon <tomekcichon@tlen.pl>
2023-11-09 16:19:09 +0100

    add login and log out chatbot message

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 const loginForm = document.getElementById("welcome-form");
 const messagesSection = document.getElementById("messages-section");
 const messagesList = document.getElementById("messages-list");
@@ -6,7 +8,6 @@ const userNameInput = document.getElementById("username");
 const messageContentInput = document.getElementById("message-content");
 
 let userName = '';
-
 
 const login = (event) => {
     event.preventDefault();
@@ -18,10 +19,12 @@ const login = (event) => {
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
         userName = userNameValue;
+        socket.emit('join', {login: userName})
     }
 }
 
-function addMessage(author, content) {
+
+const addMessage = (author, content) => {
     const message = document.createElement('li');
     message.classList.add('message');
     message.classList.add('message--received');
@@ -37,13 +40,48 @@ function addMessage(author, content) {
 
 const sendMessage = (event) => {
     event.preventDefault();
-    if (!messageContentInput.value) {
+
+    let messageContent = messageContentInput.value;
+
+    if (!messageContent.length) {
         alert('Please input message')
     } else {
         addMessage(userName, messageContentInput.value)
+        socket.emit('message', { author: userName, content: messageContent })
         messageContentInput.value = '';
     }
 }
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('newUser', (user) => newUser(user));
+socket.on('userDisconnected', (disconnectedUser) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    message.innerHTML = `
+        <h3 class="message__author">Chat Bot</h3>
+        <div class="message__content">
+            <p class="message_chatbot">${disconnectedUser} has left the conversation...</p>
+        </div>
+    `;
+    messagesList.appendChild(message);
+});
+
+
+const newUser = (user) => {
+
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    message.innerHTML = `
+      <h3 class="message__author">Chat Bot</h3>
+      <div class="message__content">
+        <p class="message_chatbot">${user} Has joined the conversation!</p>
+      </div>
+    `;
+    messagesList.appendChild(message);
+
+}
--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client')));
 
 const messages = [];
+const logins = [];
 
 app.get('/', (req, res) => {
     res.render('/index');
   });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+      });
+      socket.on('join', (login) => {
+        console.log('Oh, I\'ve got userName from ' + socket.id);
+        logins.push({ id: socket.id, user: login.login });
+        console.log('users: ', logins);
+        socket.broadcast.emit('newUser', login.login);
+    
+      });
+
+      socket.on('disconnect', () => {
+        const disconnectedUserIndex = logins.findIndex((user) => user.id === socket.id);
+        if (disconnectedUserIndex !== -1) {
+            const disconnectedUser = logins[disconnectedUserIndex];
+            logins.splice(disconnectedUserIndex, 1); // Remove the disconnected user from logins array
+            console.log('User disconnected:', disconnectedUser.login);
+            console.log('users: ', logins);
+            io.emit('userDisconnected', disconnectedUser.user);
+        }
+    });
+
+    console.log('I\'ve added a listener on message and disconnect events \n');
+  });
+
